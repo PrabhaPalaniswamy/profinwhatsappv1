68f50dbf765f300a968ecd993f871aab0abec076
PrabhaPalaniswamy <prabhajanarthanan@gmail.com>
2024-05-16 13:02:42 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/material.dart';
-//import 'package:profinwhatsappv1/pro_textfield.dart';
+import 'package:profinwhatsappv1/pro_text.dart';
 
 void main() {
   runApp(
-      const pro_text()); //const- constructor is a compile time const. it telling flutter that class myapp is no need to create again
+ pro_text()); //const- constructor is a compile time const. it telling flutter that class myapp is no need to create again
 }
 
-class pro_text extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: pro_text(),
-    );
-  }
-}
+
